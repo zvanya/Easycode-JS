f46d74d46bb2f111a05bda3f3ca47a5fdea83391
zvanya <zhakin.v@gmail.com>
2019-01-26 01:52:20 +0200

    hw9

---
+++ js/hw9/3.js
@@ -0,0 +1,41 @@
+// Деструктурирующее присваивание. Задачи.
+
+// 1. Используя rest оператор и деструктуризацию, создать функцию, которая
+//    принимает любое количество аргументов и возвращает объект,
+//    содержащий первый аргумент и массив из остатка:
+//    func(‘a’, ‘b’, ‘c’, ‘d’) →
+//    {
+//        first: ‘a’,
+//        other: [‘b’, ‘c’, ‘d’]
+//    }
+
+function f(first, ...rest) {
+    return {first, rest};
+}
+
+let obj = f('a', 'b', 'c', 'd');
+let {first: fst, rest: r} = f('a', 'b', 'c', 'd'); //fst === "a", r === ['b', 'c', 'd']
+
+// 2. Организовать функцию getInfo, которая принимает объект вида
+//    { name: ..., info: { employees: ..., partners: [ … ] } }
+//    и выводит в консоль имя (если имени нет, показывать ‘Unknown’) и первые две
+//    компании из массива employees:
+//    const organisation = {
+//        name: 'Google',
+//        info: { employees: 1536, partners: ['Microsoft', 'Facebook', 'Xing'] }
+//    };
+//    getInfo(organisation); →
+//    Name: Google
+//    Partners: Microsoft Facebook
+
+function getInfo({name, info: {employees, partners}}) {
+    console.log(`Name: ${!name ? "Unknown" : name}`);
+    console.log(`Partners: ${partners[0]}, ${partners[1]}`);
+}
+
+const organisation = {
+    name: 'Google',
+    info: { employees: 1536, partners: ['Microsoft', 'Facebook', 'Xing'] }
+};
+
+getInfo(organisation);
